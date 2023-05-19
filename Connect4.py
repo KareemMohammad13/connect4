7bfa050612331afbb4cd2b096ca6878f29d0506b
Cody-Manga <101194325+Cody-Manga@users.noreply.github.com>
2023-05-19 17:32:34 +0300

    Update Connect4.py

--- Connect4.py
+++ Connect4.py
@@ -31,3 +31,28 @@ def is_game_over(board):
 # Check if the board is full
 def is_board_full(board):
     return np.all(board != EMPTY)
+
+
+
+# Render the game board in the GUI
+def render_board():
+    for widget in game_frame.winfo_children():
+        widget.destroy()
+    for row in range(ROW_COUNT):
+        for col in range(COLUMN_COUNT):
+            color = 'blue' if board[row][col] == PLAYER else 'red' if board[row][col] == AI else 'white'
+            slot = tk.Canvas(game_frame, width=50, height=50, bg=color, highlightthickness=1, highlightbackground='black')
+            slot.grid(row=row, column=col, padx=2, pady=2)
+            slot.bind('<Button-1>', lambda e, col=col: make_player_move(col))
+
+# Create the game board and GUI
+board = create_board()
+root = tk.Tk()
+root.title('Connect Four')
+
+game_frame = tk.Frame(root)
+game_frame.pack()
+
+render_board()
+
+root.mainloop()
